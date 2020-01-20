a124162a465744b01bcb568d613a9e7ed6894933
lunaroyster <oneinaquadrillion@live.com>
2020-01-20 11:38:23 -0500

    Scaffolded app router + history

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
+
+import { Router, Route, Switch } from 'react-router-dom';
+
+import history from './utils/history';
+
+import GraphViewer from './Components/GraphViewer';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <Switch>
+          <Route path="/" exact component={GraphViewer} />
+        </Switch>
+      </Router>
     </div>
   );
 }
