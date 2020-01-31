ace75fe1b638a4ccc830c705095d16c8671f676e
lunaroyster <oneinaquadrillion@live.com>
2020-01-31 00:46:15 -0500

    Added a defaultLayout getter

--- app/src/services/Complex.js
+++ app/src/services/Complex.js
@@ -11,6 +11,19 @@ class Complex {
   get content() {
     return this._complex.content;
   }
+  get defaultLayout() {
+    /**
+     * Gets the default layout from content.
+     * 
+     * Note: This function is temporary, pending complete implementation of paths.
+     */
+    let content = this.content;
+    for (let id in content) {
+      if (new Set(content[id].meta.is).has('Layout')) {
+        return content[id];
+      }
+    }
+  }
 }
 
 export default Complex
