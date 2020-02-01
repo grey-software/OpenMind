92c1ce79d38997929ad9701bdf81f15819a19170
lunaroyster <oneinaquadrillion@live.com>
2020-02-01 06:25:04 -0500

    UI events subscription

--- app/src/services/openMindService.js
+++ app/src/services/openMindService.js
@@ -1,5 +1,8 @@
 import cytoscape from 'cytoscape';
 
+import { Subject, ReplaySubject } from 'rxjs';
+import { delay, throttleTime, filter, timeInterval, map, mergeMap } from 'rxjs/operators';
+
 import { decorate, observable, computed } from 'mobx';
 
 import Complex from './Complex';
@@ -20,6 +23,50 @@ class _OpenMindService {
       wheelSensitivity: 0.1,
     });
     this.cy.style({});
+    this.setGraphBindings();
+  }
+  setGraphBindings() {
+    /**
+     * Binds cytoscape events.
+     */
+    // Subjects
+    this.nodeTap = new Subject();
+    this.nodeMouseover = new Subject();
+    this.nodeMouseout = new Subject();
+
+    this.nodesMove = new Subject(); // node.dragfree
+
+    this.edgeTap = new Subject();
+    this.edgeMouseover = new Subject();
+
+    this.backgroundTap = new Subject();
+    this.backgroundDoubleTap = this.backgroundTap.pipe(
+      timeInterval(),
+      filter(i => i.interval < 250),
+      map(i => i.value),
+    );
+    this.backgroundContextTap = new Subject();
+
+    // Bindings to cytoscape
+
+    this.cy.on('tap', 'node', e => this.nodeTap.next(e));
+    this.cy.on('tap', 'edge', e => this.edgeTap.next(e));
+    this.cy.on('tap', e => {
+      if (e.target == e.cy) this.backgroundTap.next(e);
+    });
+    this.cy.on('cxttap', e => {
+      if (e.target == e.cy) this.backgroundContextTap.next(e);
+    });
+    this.cy.on('dragfree', 'node', e => this.nodesMove.next(e));
+
+    this.cy.on('mouseover', 'node', e => this.nodeMouseover.next(e));
+    this.cy.on('mouseout', 'node', e => this.nodeMouseout.next(e));
+    this.nodeMouseover.subscribe(e => {
+      this.cy.container().style.cursor = 'pointer';
+    });
+    this.nodeMouseout.subscribe(e => {
+      this.cy.container().style.cursor = '';
+    });
   }
   mountCyto(el) {
     /**
