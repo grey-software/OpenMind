d2ef16ad52c4516b2a26598838750c5a0609f784
lunaroyster <oneinaquadrillion@live.com>
2020-02-01 04:12:26 -0500

    Styled upload overlay

--- app/src/Components/GraphViewer/UploadOverlay.js
+++ app/src/Components/GraphViewer/UploadOverlay.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
+import { Button } from '@material-ui/core'
+
 import oms from '../../services/openMindService';
+
+import './styles.scss';
 
 class UploadOverlay extends Component {
   constructor(props) {
@@ -14,8 +18,10 @@ class UploadOverlay extends Component {
   }
   render() {
     return (
-      <div>
-        <div onClick={()=>this.fileRef.current.click()}>Upload</div>
+      <div className="uploadOverlay">
+        <div className="uploadBox">
+          <Button className="uploadButton" onClick={()=>this.fileRef.current.click()}>Upload</Button>
+        </div>
         <input type="file" ref={this.fileRef} accept="application/json" style={{"display": "none"}} onChange={this.uploadLocalFile} />
       </div>
     );
