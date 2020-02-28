ad45acd8bb13136a619b417528106ae6482c0520
ArsalaBangash <arsalabangash@gmail.com>
2020-02-27 20:46:31 -0500

    Scaffolded use cases

---
+++ packages/docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    title: 'Open Mind',
+    description: "We're creating an open standard for organizing learning and research notes using mind maps.",
+    themeConfig: {
+        nav: [
+            { text: 'Use Cases', link: '/use-cases/' }]
+    }
+}
