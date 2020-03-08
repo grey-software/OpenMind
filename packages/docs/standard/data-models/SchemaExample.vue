ce167ff6ffe78d87c732716d79dfd469bddcf655
lunaroyster <oneinaquadrillion@live.com>
2020-03-08 01:17:37 -0400

    Created new SchemaExample component

---
+++ packages/docs/standard/data-models/SchemaExample.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="true">
+    <!-- <pre><code class="language-json">{{schema.GenericEntitySchema}}</code></pre> -->
+    <Prism language="javascript">{{ schema }}</Prism>
+  </div>
+  <div v-else>
+    <Prism language="javascript">{{ example }}</Prism>
+  </div>
+</template>
+
+<script>
+import 'prismjs'
+// import 'prismjs/themes/prism.css'
+
+import Prism from 'vue-prism-component';
+
+export default {
+  props: {
+    schema: Object,
+    example: Object,
+  },
+  components: {
+    Prism,
+  }
+}
+</script>
