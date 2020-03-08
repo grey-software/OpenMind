0ab10d941d119f3b53560ffc7f0758a440b13164
lunaroyster <oneinaquadrillion@live.com>
2020-03-08 02:09:54 -0400

    Added modes to SchemaExample

--- packages/docs/standard/data-models/SchemaExample.vue
+++ packages/docs/standard/data-models/SchemaExample.vue
@@ -1,12 +1,41 @@
 <template>
-  <div v-if="true">
-    <!-- <pre><code class="language-json">{{schema.GenericEntitySchema}}</code></pre> -->
-    <Prism language="javascript">{{ schema }}</Prism>
-  </div>
-  <div v-else>
-    <Prism language="javascript">{{ example }}</Prism>
+  <div>
+    <div class="modeContainer">
+      <button :class="['modeSwitch', {active: mode==0}]" v-on:click="mode = 0">Schema</button>
+      <button :class="['modeSwitch', {active: mode==1}]" v-on:click="mode = 1">Example</button>
+    </div>
+    <div v-if="mode == MODES.schema">
+      <!-- <pre><code class="language-json">{{schema.GenericEntitySchema}}</code></pre> -->
+      <Prism language="javascript">{{ schema }}</Prism>
+    </div>
+    <div v-else-if="mode == MODES.example">
+      <Prism language="javascript">{{ example }}</Prism>
+    </div>
   </div>
 </template>
+
+<style>
+pre {
+  margin-top: 0px !important;
+  border-radius: 0 !important;
+}
+.modeContainer {
+  margin-top: 0.85rem;
+  background: #282c34;
+}
+.modeSwitch {
+  background: none;
+  color: white;
+  border: none;
+  padding: 1em;
+  font-family: monospace;
+  outline: none;
+  cursor: pointer;
+}
+.modeSwitch.active {
+  color: orange
+}
+</style>
 
 <script>
 import 'prismjs'
@@ -18,6 +47,15 @@ export default {
   props: {
     schema: Object,
     example: Object,
+  },
+  data: () => {
+    return {
+      mode: 0,
+      MODES: {
+        schema: 0,
+        example: 1,
+      },
+    }
   },
   components: {
     Prism,
