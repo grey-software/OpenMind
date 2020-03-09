7f944eb2d1f14dccf672097b0ebcd0f7e542dbaa
lunaroyster <oneinaquadrillion@live.com>
2020-03-09 12:11:52 -0400

    fixed sidebar url

--- packages/docs/.vuepress/config.js
+++ packages/docs/.vuepress/config.js
@@ -15,7 +15,7 @@ module.exports = {
                     children: [
                         '',
                         'research/',
-                        'language-learning/',
+                        'understanding-language/',
                         'learning/',
                     ]
                 }
