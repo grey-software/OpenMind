780c7a35be82168b62b0ee9448eded66860b2230
lunaroyster <oneinaquadrillion@live.com>
2020-03-08 23:58:14 -0400

    OpenMind components

---
+++ packages/OpenMindComponents/index.js
@@ -0,0 +1,12 @@
+class OMInstance {
+  get content() {
+    // return a map of {id: <content>}
+  }
+
+  get spaces() {
+    // return a map of {id: <content:space>}
+  }
+  getContentByType(type) {
+    // return a map of {id: <content:type>}
+  }
+}
