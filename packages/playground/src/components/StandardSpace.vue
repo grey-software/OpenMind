c3a873ad1efa287d3b868280d8343b3278d42043
lunaroyster <oneinaquadrillion@live.com>
2020-03-30 18:17:51 -0400

    Scaffolded standard space component

---
+++ packages/playground/src/components/StandardSpace.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+
+<script lang="ts">
+import _ from "lodash";
+
+import ContentMeta from "../types/ContentMeta";
+
+interface StandardSpaceData {
+  meta: ContentMeta;
+  data: {
+    
+  };
+}
+export default {
+  methods: {
+    updateSpace: () => {
+
+    }
+  },
+  async mounted() {},
+  computed: {},
+  watch: {
+    content: {
+      deep: true,
+      handler: async function(val, oldVal) {
+        if (val !== oldVal) await this.updateSpace();
+      }
+    }
+  },
+  props: {
+    content: {
+      type: Object as () => StandardSpaceData,
+      default: ""
+    }
+  },
+  data() {
+    return {
+    };
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
