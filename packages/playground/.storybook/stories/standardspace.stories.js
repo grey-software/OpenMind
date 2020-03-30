a68a05df1df79e5fadbf888dd23ca04e07773e48
lunaroyster <oneinaquadrillion@live.com>
2020-03-30 18:26:39 -0400

    Added standard space story

---
+++ packages/playground/.storybook/stories/standardspace.stories.js
@@ -0,0 +1,31 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
+
+import '../../src/plugins/axios.ts';
+
+import StandardSpace from '../../src/components/StandardSpace.vue'
+
+export default {
+  component: StandardSpace,
+  title: 'Standard Space',
+  decorators: [withKnobs],
+}
+
+const StandardSpaceWithContent = content => () => ({
+  components: { StandardSpace },
+  props: {
+    content: {
+      default: {
+        meta: {},
+        data: {
+          content,
+        }
+      }
+    },
+  },
+  template: `<StandardSpace :content="content" />`,
+})
+
+export const emptyStandardSpace = StandardSpaceWithContent({})
--- packages/playground/src/components/StandardSpace.vue
+++ packages/playground/src/components/StandardSpace.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    
+    {{content}}
   </div>
 </template>
 
