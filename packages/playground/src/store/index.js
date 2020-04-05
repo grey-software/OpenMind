564ded6751b1ea7a128176357c4b8326a129139f
lunaroyster <oneinaquadrillion@live.com>
2020-04-05 19:59:54 -0400

    (blank) functions to import OpenMind repositories

--- packages/playground/src/store/index.js
+++ packages/playground/src/store/index.js
@@ -3,12 +3,30 @@ import { action, computed, observable } from "mobx";
 import OpenMindRepository from './OpenMindRepository';
 
 export default class OpenMindApp {
-  db = null
+  db = null;
   loadOpenMind = {
     example: () => {
+      /**
+       * Loads an example repository, for debugging.
+       */
       this.db = new OpenMindRepository({
-        
+
       });
     },
+    github: () => {
+      /**
+       * Loads an OpenMind repository from GitHub
+       */
+    },
+    file: () => {
+      /**
+       * Loads an OpenMind from a json file
+       */
+    },
+    localStore: () => {
+      /** 
+       * Loads an OpenMind from localStore 
+       */
+    }
   }
 }
