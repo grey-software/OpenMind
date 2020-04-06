dc68b873411e51c5bdcadec4926417b62ec9a8fa
lunaroyster <oneinaquadrillion@live.com>
2020-04-06 02:39:11 -0400

    Added Text content example to exampleOms

--- packages/playground/src/store/exampleOms.js
+++ packages/playground/src/store/exampleOms.js
@@ -2,11 +2,21 @@ export default {
   id: "example-oms",
   name: "Example OpenMind Repository",
   content: {
+    'content-1': {
+      meta: {
+        is: ['Text'],
+        id: 'content-1',
+        creationTime: 1586155025575
+      },
+      data: {
+        text: "Hello world."
+      }
+    },
     'space-1': {
       meta: {
         is: ['StandardSpace', 'Space'],
         id: 'space-1',
-        creationTime: 1231006505000,
+        creationTime: 1586155025560,
       },
       data: {
         content: {
