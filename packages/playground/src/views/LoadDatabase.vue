b522f5567d3badc07acb57235f4561476d45be13
lunaroyster <oneinaquadrillion@live.com>
2020-04-05 20:43:50 -0400

    Implemented 'Load Strategies'

--- packages/playground/src/views/LoadDatabase.vue
+++ packages/playground/src/views/LoadDatabase.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="home">
-    <button @click="state.loadOpenMind.example">Example</button>
-    <button @click="state.loadOpenMind.github">GitHub</button>
-    <button @click="state.loadOpenMind.localStorage">Local storage</button>
-    <button @click="state.loadOpenMind.file">File</button>
+    <v-btn v-for="strat in loadStrategies" @click="()=>LoadWithStrategy(strat)" :key="strat.name">{{strat.name}}</v-btn>
   </div>
 </template>
 
@@ -14,8 +11,33 @@ import AppState from "../store";
 // @ is an alias to /src
 export default observer({
   data() {
+    let loadStrategies = [
+      {
+        name: "Example",
+        loader: AppState.loadOpenMind.example
+      },
+      {
+        name: "GitHub",
+        loader: AppState.loadOpenMind.github
+      },
+      {
+        name: "localStorage",
+        loader: AppState.loadOpenMind.localStorage
+      },
+      {
+        name: "File",
+        loader: AppState.loadOpenMind.file
+      }
+    ];
     return {
       state: AppState,
+      loadStrategies,
+    }
+  },
+  methods: {
+    LoadWithStrategy(strategy) {
+      strategy.loader();
+      this.$router.push('application')
     }
   },
   name: 'loadDatabase',
